adab1ba302b6240d06aaf715f955c1a93d3685df
KeabetsweMalwa <ST10442089@rcconnect.edu.za>
2025-04-01 20:25:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberSec_Awareness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ASCII Art Title Screen
+            string asciiArt = @"
+      .----------------.  
+     | .--------------. |  
+     | |  CYBERSEC   | |  
+     | |  AWARENESS  | |  
+     | |     BOT     | |  
+     | '--------------' |  
+      '----------------'  
+        [==]    [==]   
+       /~~~~\  /~~~~\   
+      | |  | || |  | |   
+      |_|  |_||_|  |_|  
+            ";
+
+            Console.WriteLine(asciiArt);
+
+
+
+        }
+    }
+}
